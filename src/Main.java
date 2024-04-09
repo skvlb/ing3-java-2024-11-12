29ba0c8d93bc964b09382935fe4a7a294c0234c0
skvlb <95703110+skvlb@users.noreply.github.com>
2024-04-09 15:55:32 +0200

    réparation main (scrr + lib) et connexion database

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
 import Modele.Billet;
 import Modele.DAO.BilletDAO_fonctions;
 import java.sql.*;
-import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
         Billet mon_billet = new Billet(1, 3, 5, 6, 20, true);
         // Déclarations des paramètres de connexion
-        String url = "jdbc:mysql://localhost:3306/cinema"; // URL de connexion
+        String url = "jdbc:mysql://localhost:3306/cinema?serverTimezone=UTC"; // Ajout de la timezone
         String utilisateur = "root"; // Nom d'utilisateur de la base de données
         String motDePasse = ""; // Mot de passe de la base de données
 
@@ -16,10 +15,11 @@ public class Main {
             Connection connexion = DriverManager.getConnection(url, utilisateur, motDePasse);
             BilletDAO_fonctions mafonction = new BilletDAO_fonctions(connexion);
             mafonction.ajouterBillet(mon_billet);
+            System.out.println("Connexion et opération réussies.");
         } catch (SQLException e) {
             // Gestion des exceptions SQL
+            System.out.println("Erreur de connexion à la base de données.");
             e.printStackTrace();
         }
     }
-
 }

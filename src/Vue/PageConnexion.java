5dd4303f1d63cee8ab4bea556d79d66dd6dcc6b3
skvlb <95703110+skvlb@users.noreply.github.com>
2024-04-12 10:46:40 +0200

    premier test interface

--- src/Vue/PageConnexion.java
+++ src/Vue/PageConnexion.java
@@ -1,20 +1,16 @@
 package Vue;
 
 import javax.swing.*;
-import java.awt.event.*;
 
 public class PageConnexion extends JPanel {
     private PagePrincipale pagePrincipale;
 
     public PageConnexion(PagePrincipale pagePrincipale) {
         this.pagePrincipale = pagePrincipale;
-        JButton boutonConnexion = new JButton("Connexion");
-        boutonConnexion.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                pagePrincipale.afficherPageAccueil();
-            }
-        });
+        // Configurer le layout et ajouter des composants pour la connexion ici
 
+        JButton boutonConnexion = new JButton("Connexion");
+        boutonConnexion.addActionListener(e -> pagePrincipale.afficherPageAccueil()); // Après connexion, revenir à la page principale
         add(boutonConnexion);
     }
 }

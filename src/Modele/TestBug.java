e1c36399e241eea08cee46878a49fd3f745e1dd9
karl7lk <karl.lavocat@gmail.com>
2024-04-18 14:51:36 +0200

    karl getAllfilms

--- src/Modele/TestBug.java
+++ src/Modele/TestBug.java
@@ -2,13 +2,39 @@ package Modele;
 
 import Modele.DAO.DaoFactory;
 import Modele.DAO.UtilisateurDAO;
+import Modele.*;
+import Modele.DAO.*;
 
+import java.sql.Date;
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.List;
 public class TestBug {
     public static void main(String[] args) {
         DaoFactory daoFactory = DaoFactory.getInstance();
-        TypeClient type_client = new TypeClient(4, "il aime les fleurs", 0.004);
-        Client mon_client = new Client(67, "loulou", "Daniel", "gerard@gmail.com", "swiduheu", "34555", type_client);
-        UtilisateurDAO utilisateurDAO = daoFactory.getUtilisateurDAO();
-        utilisateurDAO.ajouterUtilisateur(mon_client);
+        FilmDAO filmDAO = daoFactory.getFilmDAO();
+        Film mon_film = new Film(3,"blabla",15,"LILIAN","C:\\Projet_Java_ING3\\Matriximg.jpg");
+        Film mon_film2 = new Film(6,"bloublou",12,"LAlo","C:\\Projet_Java_ING3\\Matriximg.jpg");
+        filmDAO.ajouterFilm(mon_film);
+        filmDAO.ajouterFilm(mon_film2);
+        List<Film> film = new ArrayList<>();
+        film=filmDAO.getAllFilms();
+        /*Time heureDebut = new Time(10, 0, 0);
+// Créer une heure de fin (par exemple 12:30:00)
+        Time heureFin = new Time(12, 30, 0);
+        Time heureDebut2     = new Time(14, 0, 0);
+// Créer une heure de fin (par exemple 12:30:00)
+        Time heureFin2 = new Time(16, 30, 0);
+        Date date1 = Date.valueOf("2024-04-17");
+        Programmation ma_programmation = new Programmation(4,3,4,heureDebut,heureFin,date1);
+        Programmation ma_programmation2 = new Programmation(5,3,4,heureDebut2,heureFin2,date1);
+        ProgrammationDAO programmationDAO = daoFactory.getProgrammationDAO();
+        programmationDAO.ajouterProgrammation(ma_programmation);
+        programmationDAO.ajouterProgrammation(ma_programmation2);
+        List<Programmation> programmations = new ArrayList<>();
+        programmations=programmationDAO.getProgrammationParIdFilm(3);
+        */for(int i= 0;i<film.size();i++){
+            System.out.println(film.get(i).getAuteur());
+        }
     }
 }

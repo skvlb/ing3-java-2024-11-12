aab2a27c655ae1bc6060a84d1a611647341b815d
Kant1R12 <quentin-richard@hotmail.fr>
2024-04-18 22:00:38 +0200

    bin et page crea

--- src/Vue/Page_Creation_utilisateur.java
+++ src/Vue/Page_Creation_utilisateur.java
@@ -1,8 +1,15 @@
 package Vue;
 
-import javax.swing.*;
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Page_Creation_utilisateur extends JPanel {
     private JTextField nomField;

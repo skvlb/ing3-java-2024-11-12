892df752268b1bb81294f4ac5c49fce6dc8e82d4
Kant1R12 <quentin-richard@hotmail.fr>
2024-04-20 00:34:27 +0200

    tarif prix

--- src/Controleur/TarifsControleur.java
+++ src/Controleur/TarifsControleur.java
@@ -2,8 +2,22 @@ package Controleur;
 
 public class TarifsControleur {
     public static void traiterSelectionBoutonRadio(String selection) {
-        if(selection != ""){
+        double prix = 10;
+        if(!selection.equals("")){
             System.out.println("Sélection du bouton radio : " + selection);
+            
+            // Enfant, Etudiant et Normal
+
+            if(selection.equals("Etudiant")){
+                prix = prix * 0.75;
+            }
+            else if(selection.equals("Enfant")){
+                prix = prix * 0.5;
+            }
+            System.out.println(selection + "    :   " + prix);
+
+
+
 
             // CHANGER DE PAGE (Page paiement)
 

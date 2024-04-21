d27b6f4ffde1ce0bfa9f0444d28308bb7dea8117
skvlb <95703110+skvlb@users.noreply.github.com>
2024-04-21 19:25:18 +0200

    commentaires

--- src/Vue/PanierPage.java
+++ src/Vue/PanierPage.java
@@ -8,6 +8,7 @@ import Modele.DAO.ProgrammationDaoImpl;
 import Modele.DAO.UtilisateurDaoImpl;
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
 
 public class PanierPage extends JPanel {
     private DaoFactory daoFactory;
@@ -19,24 +20,23 @@ public class PanierPage extends JPanel {
     }
 
     private void setupUI(Billet billet) {
-        setLayout(new GridLayout(7, 1, 10, 10)); // Mise à jour du GridLayout pour inclure toutes les lignes
+        setLayout(new GridLayout(7, 1, 10, 10)); 
         setBackground(Color.WHITE);
         
-        // Utiliser UtilisateurDao pour récupérer l'email de l'utilisateur via son ID
+        //  UtilisateurDao pour récupérer l'email de l'utilisateur via son ID
         UtilisateurDaoImpl utilisateurDao = new UtilisateurDaoImpl(daoFactory);
         String email = utilisateurDao.getEmailUtilisateurParId(billet.getId_client());
         
-        // Utiliser ProgrammationDao pour récupérer les détails de la programmation
+        //ProgrammationDao pour récupérer les détails de la programmation
         ProgrammationDaoImpl programmationDao = new ProgrammationDaoImpl(daoFactory);
         Programmation programmation = programmationDao.getProgrammationParId(billet.getId_programmation());
         
-        // Ajouter les composants avec les informations récupérées
         add(new JLabel("Email Client: " + email));
         add(new JLabel("Film: " + programmation.getFilmId())); 
         add(new JLabel("Date: " + programmation.getDate().toString()));
         add(new JLabel("Heure de début: " + programmation.getHeureDebut().toString()));
         add(new JLabel("Salle: " + programmation.getSalleId()));
-        add(new JLabel("Siège N°: " + siegeNumero)); // Ajouter le numéro de siège au modèle Billet si nécessaire
+        add(new JLabel("Siège N°: " + siegeNumero)); 
         add(new JLabel("Prix: " + billet.getPrix() + " EUR"));
 
         JButton validerAchatButton = new JButton("Valider l'achat");
@@ -45,8 +45,16 @@ public class PanierPage extends JPanel {
     }
 
     private void validerAchat(Billet billet) {
+        int randomBilletId = genererIdBilletAleatoire();
+        billet.setId_billet(randomBilletId);
         BilletDaoImpl billetDao = new BilletDaoImpl(daoFactory);
         billetDao.ajouterBillet(billet);
-        JOptionPane.showMessageDialog(this, "Achat validé pour le billet ID: " + billet.getId_billet());
+        JOptionPane.showMessageDialog(this, "Achat validé ");
+    }
+
+    private int genererIdBilletAleatoire() {
+        Random rand = new Random();
+        int randomId = rand.nextInt(Integer.MAX_VALUE);
+        return randomId;
     }
 }
